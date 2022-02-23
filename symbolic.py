4fb17487c4cd42cea6ea38bfb2757182329e04a3
Bruce Rose <brucej.rose@orange.fr>
2022-02-23 18:07:08 +0100

    Gave up on symbolic.py; adopted deap implementation; wip

--- symbolic.py
+++ symbolic.py
@@ -1,22 +1,78 @@
 from math import sin, cos, sqrt, log
+from random import choice, random
+
+
+"""We will use deap instead"""
 
 class Symbolic():
     func_set = {
-        "pow" : lambda x, y : pow(x, y),
-        "+" : lambda x, y : x + y,
-        "-" : lambda x, y : x - y,
-        "*" : lambda x, y : x * y,
-        "1/" : lambda x : 1./x,
-        "sin" : lambda x : sin(x),
-        "cos" : lambda x : cos(x),
-        "sqrt" : lambda x: sqrt(x),
-        "log" : lambda x: log(x),
+        "pow" : (2, lambda x, y : pow(x, y)),
+        "+" : (2, lambda x, y : x + y),
+        "-" : (2, lambda x, y : x - y),
+        "*" : (2, lambda x, y : x * y),
+        "1/" : (1, lambda x : 1./x),
+        "sin" : (1, lambda x : sin(x)),
+        "cos" : (1, lambda x : cos(x)),
+        "sqrt" : (1, lambda x: sqrt(x)),
+        "log" : (1, lambda x: log(x)),
     } 
 
-    def __init__(self, input_variables, output_size, maxh=5):
-        self.input_variables = input_variables
+    def __init__(self, input_size, output_size, maxsize=10):
+        # variables
+        # vnumber:cpt:h
+        # v1:1:3
+
+        # constant
+        # c:cpt:h
+
+        # Undefined
+        # N:cpt:h
+
+        # float
+        # f:cpt:h
+        # int
+        # i:cpt:h
+
+        self.input_variables = ["v"+str(i) for i in range(input_size)]
         self.output_size = output_size
-        self.maxh = maxh
+        self.maxsize = maxsize
+        self.tree = {}
+        # cpt : int, current number of nodes
+        cpt = 0
+        ## init
+        for i in range(self.output_size):
+            f = choice(Symbolic.func_set.keys())
+            arity = Symbolic.func_set[f][0]
+            cpt += 1
+            self.tree[f+":"+str(cpt)+":0"] = []
+            ## Create undefined nodes
+            for j in range(arity):
+                cpt += 1
+                self.tree[f].append("N:"+str(cpt)+":1")
+
+        ## generation loop
+        done = False
+        while not done:
+            done = True
+            for k in self.tree.keys():
+                for i in range(len(self.tree[k])):
+                    v = self.tree[k][i]
+                    if v.split(":")[0] == "N":
+                        # chance to fuse with another node
+                        if random() < 0.3:
+                            # fuse
+                            # find other none
+
+                        # Variable, constant or function ?
+                        if choice([True, False]) or len(self.tree.keys()) >= self.maxsize:
+                            # Variable/constant
+                            if choice([True, False]):
+                                # variable
+                                self.tree[choice(self.input_variables)]
+                            else:
+                                # constant
+                        else:
+                            # function
 
 
     def show(self):
--- trader.py
+++ trader.py
@@ -1,9 +1,75 @@
+# from symbolic import Symbolic
+from deap import PrimitiveSetTyped
+import operator
+import random
 class Trader():
     nb_traders = 0
+    horizon = 1
+    def if_then_else(input, output1, output2):
+        return output1 if input else output2
+    # TODO determine a pset for each decision (buy/sell, vol, price)
+    # TODO choose functions to add to pset
+    # TODO think about types of functions and return
+    # DONE solve buy/sell/donothing problem
+    # because the output of the tree which decide wether or not to buy/sell/do nothing is of size 3. How can a mathematical expression return something with arity 3?
+    # arity 2 is easy: negative: choice 1, positive: choice 2
+    # ok when you think about it, we can define a zone around zero in which we do nothing
+    # so output is float
+
+    neutral_zone = 10
+
+    # input is sell_price, sell_vol, buy_price, buy_vol, and open, close, high, low, vol=nb of transactions * h
+    pset_buy = PrimitiveSetTyped("main", [float, int, float, int] + [float, float, float, float, int]*horizon, float)
+    # pset_buy.addPrimitive(operator.xor, [bool, bool], bool)
+    pset_buy.addPrimitive(operator.add, [float, float], float)
+    pset_buy.addPrimitive(operator.sub, [float, float], float)
+    pset_buy.addPrimitive(operator.mul, [float, float], float)
+    pset_buy.addPrimitive(if_then_else, [bool, float, float], float)
+    pset_buy.addPrimitive(operator.not_, [bool], bool)
+    pset_buy.addPrimitive(operator.abs, [float], float)
+    pset_buy.addPrimitive(operator.and_, [bool, bool], bool)
+    pset_buy.addPrimitive(operator.floordiv, [float, float], float)
+    pset_buy.addPrimitive(operator.neg, [float], float)
+    pset_buy.addPrimitive(operator.or_, [bool, bool], bool)
+    pset_buy.addPrimitive(operator.truediv, [float, float], float)
+    pset_buy.addPrimitive(operator.xor, [bool, bool], bool)
+    pset_buy.addPrimitive(operator.lt, [float, float], bool)
+    pset_buy.addPrimitive(operator.le, [float, float], bool)
+    pset_buy.addPrimitive(operator.eq, [float, float], bool)
+    pset_buy.addPrimitive(operator.ne, [float, float], bool)
+    pset_buy.addPrimitive(operator.ge, [float, float], bool)
+    pset_buy.addPrimitive(operator.gt, [float, float], bool)
+
+    pset_buy.addEphemeralConstant(lambda: random.random()*20 - 10, float)
+
+    # pset_buy.addTerminal(3.0, float)
+    # pset_buy.addTerminal(1, bool)
+
+    # pset_buy.renameArguments(ARG0="sell_price")
+    # pset_buy.renameArguments(ARG1="sell_vol") # TODO problem... we can't rename args without knowing how many we have
+
     def __init__(self, budget):
         Trader.nb_traders += 1
         self.name = "trader" + str(Trader.nb_traders)
         self.budget = budget
+        ## randomize and generate symbolic genotype
+        # gen_buy will decide wether to buy or sell or do nothing
+        self.gen_buy = ??
+        self.gen_vol = ??
+        self.gen_price = ??
 
     def act(self, sell_price, sell_vol, buy_price, buy_vol, history):
+        # input size : 4 + 5 * h with h the horizon
+        # output : sell or buy, volume, price
+        # outputsize : 3
+
+        # what preprocessing?
+
+        # Neural network?
+        # Bayesian network?
+        # decision tree?
+        # symbolic regression? => let's go
+        
+        
+        
         pass
